b2d585fbde4da176ace4de25d3b420402dfcdd5a
David Oudenaarden <david.oudenaarden@outlook.com>
2022-12-20 18:41:01 +0100

    changes views and layouts

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("/", function () {
+    return view("auth/login");
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get("/dashboard", function () {
+    return view("dashboard");
+})
+    ->middleware(["auth", "verified"])
+    ->name("dashboard");
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware("auth")->group(function () {
+    Route::get("/profile", [ProfileController::class, "edit"])->name(
+        "profile.edit"
+    );
+    Route::patch("/profile", [ProfileController::class, "update"])->name(
+        "profile.update"
+    );
+    Route::delete("/profile", [ProfileController::class, "destroy"])->name(
+        "profile.destroy"
+    );
 });
 
 //Eigen routes
-Route::get('/index', function(){
-    return view('index');
-}); 
+Route::get("/index", function () {
+    return view("index");
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . "/auth.php";
