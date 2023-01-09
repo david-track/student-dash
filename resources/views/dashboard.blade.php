35a5e5aac4828a4c9d946b0e79ede4d61ee9528f
david-track <david.oudenaarden@outlook.com>
2023-01-09 19:32:17 +0100

    minor changes layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,5 @@
 <x-app-layout>
-    {{-- 
-    <div class="grid justify-items-center grid-cols-3 gap-4">
-        <div class="h-80">01</div>
-        <div class="h-80">02</div>
-        <div class="h-80">03</div>
-        <div class="h-80">04</div>
-        <div class="h-80">05</div>
-        <div class="h-80">06</div>
-
-    </div> --}}
-
+    
     <div class="grid grid-cols-3 gap-4">
         <div class="col-span-1 row-span-1 bg-gray-200 h-72 flex items-center justify-center">Item 1</div>
         <div class="col-span-1 row-span-1 bg-gray-200 h-32 flex items-center justify-center">Item 2</div>
