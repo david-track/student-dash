3cbeeb26c7af67da928e9852ac551fc8cd85da86
David Oudenaarden <david.oudenaarden@outlook.com>
2023-01-09 18:37:57 +0100

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-
+    {{-- 
     <div class="grid justify-items-center grid-cols-3 gap-4">
         <div class="h-80">01</div>
         <div class="h-80">02</div>
@@ -8,6 +8,16 @@
         <div class="h-80">05</div>
         <div class="h-80">06</div>
 
+    </div> --}}
+
+    <div class="grid grid-cols-3 gap-4">
+        <div class="col-span-1 row-span-1 bg-gray-200 h-72 flex items-center justify-center">Item 1</div>
+        <div class="col-span-1 row-span-1 bg-gray-200 h-32 flex items-center justify-center">Item 2</div>
+        <div class="col-span-1 row-span-1 bg-gray-200 h-32 flex items-center justify-center">Item 3</div>
+        <div class="col-span-1 row-span-1 bg-gray-200 h-72 flex items-center justify-center">Item 4</div>
+        <div class="col-span-1 row-span-1 bg-gray-200 h-32 flex items-center justify-center">Item 5</div>
+        <div class="col-span-1 row-span-1 bg-gray-200 h-32 flex items-center justify-center">Item 6</div>
     </div>
+
 
 </x-app-layout>
